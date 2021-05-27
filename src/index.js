915a4515ab92a85863bd4fa7d3d8882ec76f4277
Zhuoliang Pu <zhpu@ucsd.edu>
2021-05-27 21:27:11 +0800

    Update

--- src/index.js
+++ src/index.js
@@ -17,14 +17,6 @@ require('./index.css').toString();
  */
 class SimpleImage {
   /**
-   * Allow to press Enter inside the Alert block
-   * @public
-   * @returns {boolean}
-   */
-  static get enableLineBreaks() {
-    return true;
-  }
-  /**
    * Render plugin`s main Element and fill it with saved data
    *
    * @param {{data: SimpleImageData, config: object, api: object}}
@@ -111,7 +103,15 @@ class SimpleImage {
       icon: '<svg width="17" height="15" viewBox="0 0 336 276" xmlns="http://www.w3.org/2000/svg"><path d="M291 150V79c0-19-15-34-34-34H79c-19 0-34 15-34 34v42l67-44 81 72 56-29 42 30zm0 52l-43-30-56 30-81-67-66 39v23c0 19 15 34 34 34h178c17 0 31-13 34-29zM79 0h178c44 0 79 35 79 79v118c0 44-35 79-79 79H79c-44 0-79-35-79-79V79C0 35 35 0 79 0z"/></svg>'
     };
   }
-
+  
+  /**
+   * Allow to press Enter inside the Alert block
+   * @public
+   * @returns {boolean}
+   */
+   static get enableLineBreaks() {
+    return true;
+  }
   /**
    * Creates a Block:
    *  0) Show a button if there is no image
